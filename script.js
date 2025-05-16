cc2ca055aa4a65317c9f89ff106de9928e06047f
Saikat De <81856219+Saikat2000@users.noreply.github.com>
2025-05-16 19:49:54 +0530

    feat: Add event listeners to open and close task form
    -Toggle Task Form visibility on open and discard actions
    -Show confirmation dialog when trying to close the form
    -Close the dialog when "Cancel" or "Discard" button is clicked
    -Ensures user don't accidentally discard unsaved input

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+
+openTaskFormBtn.addEventListener("click", () =>
+    taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+    confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+});
