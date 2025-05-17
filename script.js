ed5c80884f514e8dccb546a57dc07cb9a99654f7
Saikat De <81856219+Saikat2000@users.noreply.github.com>
2025-05-17 19:35:05 +0530

    chore: Define variables to store data

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
+const taskData = [];
+let currentTask = {};
 
 openTaskFormBtn.addEventListener("click", () =>
     taskForm.classList.toggle("hidden")
